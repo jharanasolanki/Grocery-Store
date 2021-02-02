d1f13764d2fbbbf44b7d27f99089eff4ce6e0627
jharanasolanki <jharanasolanki@gmail.com>
2021-02-02 18:27:22 +0530

    database

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "remotemysql.com";
 $username = "HCT4UbGiki";
-$password = "HCT4UbGiki";
+$password = "WszXABOU7F";
 $dbname = "HCT4UbGiki";
 
 // Create connection
@@ -9,6 +9,4 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
-  
 }
-?>
