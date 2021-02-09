4cc7cb1288e514cfcd983b9c5fb80271b2b30d4c
jharanasolanki <jharanasolanki@gmail.com>
2021-02-09 11:33:38 +0530

    forgot password

--- forgotpasswordfront.php
+++ forgotpasswordfront.php
@@ -20,19 +20,26 @@
     if (isset($_SESSION['username'])) {
         header('Location:'.'index.php');
     }
+    if (isset($_GET['err'])) {
+        if ($_GET['err'] == 1) {
+            echo "<script>alert('Security Answer Incorrecct!');</script>";
+        }
+        if ($_GET['err'] == 2) {
+            echo "<script>alert('Username does not exist!');</script>";
+        }
+    }
     ?>
     <!-- Nav bar finishh -->
     <!-- Inspection form start -->
 
     <section>
         <div class="container center">
-            <form enctype="multipart/form-data" class="formoid-flat-green" style="background-color:#fff;font-size:14px;font-family:'Lato', sans-serif;color:#000;max-width:600px;min-width:150px" method="post" action="questions.php">
+            <form enctype="multipart/form-data" class="formoid-flat-green" style="background-color:#fff;font-size:14px;font-family:'Lato', sans-serif;color:#000;max-width:600px;min-width:150px" method="get" action="questions.php">
                 <div class="title">
                     <h2 style="margin-right: 27px;margin-left: 27px;">Forgot Password</h2>
                 </div>
                 <div lass="element-name">
                     <label class="title">Enter your registered username:</label> <input type="text" name="uname" required="required"/>
-                    <label class="title" id="question"></label> 
                     
                     
                     <h3><label class="subtitle" style="color:black;"><a href="signup.php">New user? Sign up!</a></label></h3>
@@ -48,33 +55,5 @@
     <!-- footer section finish -->
     </div>
 </body>
-
-<script type="text/javascript" language="javascript">
-    //this function takes username entered by the user as argument and
-    //checks wether that username is available
-    function checkEmail(str) {
-        if (str.length != 0) {
-            var xmlhttp = new XMLHttpRequest();
-            //when the open() function state is changed this function is called
-            xmlhttp.onreadystatechange = function() {
-                //checks if the open() function state is changed to complete
-                if (this.readyState == 4 && this.status == 200) {
-                    allowed = (this.responseText); // stores answer returned by open function
-                    alert(allowed);
-                    if(allowed=="0")
-                        document.getElementById("errMsg").innerHTML = "Username not registered";
-                    else
-                        document.getElementById("errMsg").innerHTML = allowed+"<input type='text' name='answer' required='required' />";
-                }
-            };
-            // the username entered by the user is sent to the php script as a querystring
-            xmlhttp.open("GET", "getquestion.php?q=" + str, true);
-            xmlhttp.send();
-        }
-    }
-
-    </script>
-
-
 
 </html>
