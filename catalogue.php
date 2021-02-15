4dab8c0aac65dc6892c06e9d51f052144e08e3d6
jharanasolanki <jharanasolanki@gmail.com>
2021-02-15 15:42:53 +0530

    catalog

---
+++ catalogue.php
@@ -0,0 +1,59 @@
+<?php require 'nav.php' ?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/formoid-flat-green.css" type="text/css" />
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/font-awesome.css">
+    <link rel="stylesheet" href="css/style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body class="hero-image">
+    <section class="section" id="trainers">
+        <div class="container">
+            <br>
+            <br>
+
+            <div class="row">
+
+                <div class="col-lg-4">
+                    <div class="trainer-item">
+                        <div class="image-thumb">
+                            <img src="images/oliveoil1.jpg" alt="" height="300px" width="300px">
+                        </div>
+                        <div class="down-content"><br>
+                        <h4>Olive Oil</h4>
+                            <span>
+                                Rs.170.00
+                            </span>
+                            <ul class="social-icons">
+                                <li><a href="product-details.html">+ Order</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="trainer-item">
+                        <div class="image-thumb">
+                            <img src="images/oliveoil2.jpg" alt="" height="300px" width="300px">
+                        </div>
+                        <div class="down-content"><br>
+                        <h4>Olive Oil</h4>
+                            <span>
+                                Rs.190.00
+                            </span>
+                            <ul class="social-icons">
+                                <li><a href="product-details.html">+ Order</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+
+</html>
